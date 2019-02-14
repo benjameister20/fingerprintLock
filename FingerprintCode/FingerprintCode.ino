59cee7ab5ee304bf71133dcb983c0f2e3f86acc9
Benjamin Lawrence <benjaminlawrence@Benjamins-MacBook-Pro.local>
2019-02-13 19:26:57 -0500

    initial code with not compilation bugs

--- FingerprintCode/FingerprintCode.ino
+++ FingerprintCode/FingerprintCode.ino
@@ -17,6 +17,8 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
 
+bool locked;
+
 void setup() {
   // begin serial monitor
   Serial.begin(9600);
@@ -27,12 +29,8 @@ void setup() {
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   delay(10);
-  pwm.setPWM(1, 0, servoPos[0]);
-  pwm.setPWM(2, 0, servoPos[1]);
-  pwm.setPWM(3, 0, servoPos[2]);
-  pwm.setPWM(4, 0, servoPos[3]);
-  pwm.setPWM(5, 0, servoPos[4]);
-  pwm.setPWM(6, 0, servoPos[5]);
+  pwm.setPWM(1, 0, 100);
+  pwm.setPWM(2, 0, 600);
   Serial.println("Sucessfully initialized motors.");
 
   // initialize fingerprint sensor
@@ -48,87 +46,57 @@ void setup() {
   Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
   Serial.println("Sucessfully initialized fingerprint sensor.");
 
+  locked = false;
+  
   Serial.println("Starting lock program...");
-}
-
-void moveServo(int xy, int joystick, int dir) {
-
-  int servo = servos[xy+joystick];        // determine which servo to move
-  int pos = servoPos[xy+joystick];        // find current position
-  int newPos = pos+dir*2;                 // calculate new position
-  //if (servo == 1)
-    //newPos += 50*dir;
-  Serial.print("Servo: ");
-  Serial.print(servo);
-  Serial.print(" and newPos: ");
-  Serial.println(newPos);
-  
-  if (newPos < SERVOMAX-10 && newPos > SERVOMIN + 10) {     // if possible then move
-    Serial.println("In if Statement");
-    Serial.print("Servo: "); Serial.print(servo);Serial.print(" newPos: ");Serial.print(newPos);Serial.print(" xy+joystick: ");Serial.println(xy+joystick);
-    pwm.setPWM(servo, 0, newPos);
-    servoPos[xy+joystick] = newPos;           // set current position as the new position
-    delay(10);
-  }
-  
+  delay(50);
 }
 
 void loop() {
 
-  // x movement
-  if (analogRead(RJVRx) > 550) {
-    moveServo(0, 2+RJState, 1);
-    Serial.print("RJVRx: ");
-    Serial.println(analogRead(RJVRy));
-  }
-  if (analogRead(RJVRx) < 480) {
-    moveServo(0, 2+RJState, -1);
-    Serial.print("RJVRx: ");
-    Serial.println(analogRead(RJVRx));
-  }
-  if (analogRead(LJVRx) > 550) {
-    moveServo(0, 1, 1);
-    Serial.print("LJVRx: ");
-    Serial.println(analogRead(LJVRx));
-  }
-  if (analogRead(LJVRx) < 480) {
-    moveServo(0, 1, -1);
-    Serial.print("LJVRx: ");
-    Serial.println(analogRead(LJVRx));
-  }
-
-  // y movement
-  if (analogRead(RJVRy) > 550) {
-    moveServo(1, 2+RJState, 1);
-    Serial.print("RJVRy: ");
-    Serial.println(analogRead(RJVRy));
-  }
-  if (analogRead(RJVRy) < 480) {
-    moveServo(1, 2+RJState, -1);
-    Serial.print("RJVRy: ");
-    Serial.println(analogRead(RJVRy));
-  }
-  if (analogRead(LJVRy) > 670) {
-    moveServo(-1, 1, 1);
-    Serial.print("LJVRy: ");
-    Serial.println(analogRead(LJVRy));
-  }
-  if (analogRead(LJVRy) < 600) {
-    moveServo(-1, 1, -1);
-    Serial.print("LJVRy: ");
-    Serial.println(analogRead(LJVRy));
-  }
-
-  // switch which motors are used if button is pressed
-  if (digitalRead(RJSWButton) == LOW && (millis() - timer > 500) ) {
-    Serial.println("button changing state");
-    if (RJState == 0)
-      RJState = 2;
+  int id = getFingerprintIDez();
+  if (id != -1) {
+    Serial.println("Valid fingerprint detected.");
+    if (locked)
+      Serial.println("The door is now unlocked.");
     else
-      RJState = 0;
-    delay(20);
-    timer = millis();
+      Serial.println("The door is now locked.");
+    delay(10);
+    
+    changeState();
   }
+   
+  delay(50);
   
-  delay(10);
+}
+
+void changeState() {
+  if (locked) {
+    pwm.setPWM(1, 0, 100);
+    pwm.setPWM(2, 0, 600);
+  }
+  else {
+    pwm.setPWM(1, 0, 600);
+    pwm.setPWM(2, 0, 100);
+  }
+
+  locked = !locked;
+}
+
+// function taken from Adafruit fingerprint tutorial
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
 }
